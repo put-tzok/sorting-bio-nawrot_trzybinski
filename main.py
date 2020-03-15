6f4b4542e3549788030103ad62a8e85ee8566d09
Sebastian Nawrot <nawrot2137@gmail.com>
2020-03-15 20:09:25 +0100

    lol2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from time import process_time
+from time import time
 from random import randrange
 
 
@@ -37,10 +37,6 @@ def selection_sort(data):
     data[index], data[selection_index] = data[selection_index], data[index]
   return data
 
-
-import sys
-
-
 def insertion_sort(data):
   for index, value in enumerate(data[1:]):
     while index >= 0 and value < data[index] : 
@@ -49,26 +45,58 @@ def insertion_sort(data):
     data[index + 1] = value 
   return data
 
+def partition(data, low, high):
+  for inner_index in range(low + 1, high):
+    if data[inner_index] <= data[high]:
+      low += 1
+      data[low], data[inner_index] = data[inner_index], data[low]
+  data[low + 1], data[high] = data[high], data[low + 1]
+  return low + 1
 
-def quick_sort(data):
-  pass
+def quick_sort(data, low=None, high=None):
+  if low is None and high is None:
+    return quick_sort(data, 0, len(data) - 1)
+  if low < high:
+    index = partition(data, low - 1, high)
+    quick_sort(data, low, index - 1)
+    quick_sort(data, index + 1, high)
+  return data
+
+def heapify(data, high, low): 
+  largest = low
+
+  left = 2 * low + 1
+  if left < high and data[low] < data[left]:
+    largest = left
+
+  right = 2 * low + 2
+  if right < high and data[largest] < data[right]:
+    largest = right
+
+  if largest != low:
+    data[low], data[largest] = data[largest], data[low]
+    heapify(data, high, largest)
 
 def heap_sort(data):
-  pass
-
+  for index in range(len(data), -1, -1):
+    heapify(data, len(data), index)
+  for index in range(len(data) - 1, 0, -1):
+    data[index], data[0] = data[0], data[index]
+    heapify(data, index, 0)
+  return data
 
 # main
 for sort_function in [selection_sort, insertion_sort, quick_sort, heap_sort]:
   for fill_function in [fill_random, fill_increasing, fill_decreasing]:
     data = fill_function(10)
 
-    start = process_time()
+    start = time()
     data = sort_function(data)
-    end = process_time()
+    end = time()
     print(end - start)
 
 
 
     if data:
       is_sorted(data)
-    print(f'{sort_function.__name__}\t{fill_function.__name__} {end - start:8.8} {data}')
+    print(f'{sort_function.__name__}\t{fill_function.__name__} {end - start:.8} {data}')
